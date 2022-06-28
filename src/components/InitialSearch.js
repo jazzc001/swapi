824129ebecea2c09ec4f6b0ddb39ffb4566853d4
jazzc001 <jazzcheung212@gmail.com>
2022-06-28 22:14:52 +0100

    get everything up to date

---
+++ src/components/InitialSearch.js
@@ -0,0 +1,10 @@
+import { } from '../App';
+
+const initialSearch = () => {
+    const [startShipSearch, setStartShipSearch] = useState('');
+    const [startShipFound, setStartShipFound] = useState([]);
+
+    
+}
+
+export default initialSearch;
